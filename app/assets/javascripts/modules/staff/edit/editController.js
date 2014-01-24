b34260d311b06d342615865a164895ea8b38ede2
JC <jorge.e.colindres@gmail.com>
2014-01-24 12:16:13 -0500

    refactor staffer model sync event to be listened to using Marionette's baked in modelEvents for the view

--- app/assets/javascripts/modules/staff/edit/editController.js
+++ app/assets/javascripts/modules/staff/edit/editController.js
@@ -3,10 +3,8 @@ App.module('StaffModule.Edit', function(Edit, App, Backbone, Marionette, $, _){
   Edit.Controller = {
     editStaffer: function(id, staffer){
       staffer = staffer || App.request('staffer:getStaffer', id);
-      staffer.on('sync', function(){
-        var editView = this.getEditView(staffer);
-        App.mainRegion.show(editView);
-      }, this);
+      var editView = this.getEditView(staffer);
+      App.mainRegion.show(editView);
     },
     getEditView: function(staffer){
       return new Edit.editStafferView({
--- app/assets/javascripts/modules/staff/edit/editView.js
+++ app/assets/javascripts/modules/staff/edit/editView.js
@@ -1,7 +1,10 @@
 App.module('StaffModule.Edit', function(Edit, App, Backbone, Marionette, $, _){
 
   Edit.editStafferView = App.Views.MainItemView.extend({
-    template: 'staff/edit/edit-staffer'
+    template: 'staff/edit/edit-staffer',
+    modelEvents: {
+      'sync':  'render'
+    }
   });
 
 });
